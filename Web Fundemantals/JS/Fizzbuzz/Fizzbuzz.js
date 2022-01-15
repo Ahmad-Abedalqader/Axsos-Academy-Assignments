dcf61c25be22056be6a99b81fdcaae2f43cecc94
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-01-15 11:47:39 +0200

    Fizzbuzz Assignment

---
+++ Web Fundemantals/JS/Fizzbuzz/Fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz() {
+    for (var i = 1; i<= 100; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            console.log("Fizzbuzz");
+        }
+        else if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if(i%5==0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+fizzbuzz();
