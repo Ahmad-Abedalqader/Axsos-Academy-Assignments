73db3722e4f592dbf5e33f60894a5c278030ab43
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-01-17 00:41:04 +0200

    Likes Assignment

---
+++ Web Fundemantals/JS/Likes/Likes-part1.js
@@ -0,0 +1,6 @@
+var count=3;
+var countelement= document.querySelector("#counter")
+function addlike(){
+    count++;
+    countelement.innerText= count + " like(s)";
+}
---
+++ Web Fundemantals/JS/Likes/Likes-part2.js
@@ -0,0 +1,24 @@
+var count1=9;
+var countelement1= document.querySelector("#counter1")
+function addlike1(){
+    count1++;
+    countelement1.innerText= count1 + " like(s)";
+}
+
+
+var count2=12;
+var countelement2= document.querySelector("#counter2")
+
+function addlike2(){
+    count2++;
+    countelement2.innerText= count2 + " like(s)";
+}
+
+
+var count3=9;
+var countelement3= document.querySelector("#counter3")
+
+function addlike3(){
+    count3++;
+    countelement3.innerText= count3 + " like(s)";
+}
