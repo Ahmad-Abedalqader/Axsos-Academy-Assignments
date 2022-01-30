fd46c78fab348f9bb90ac5a6e166666ed6cbf9b4
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-01-31 01:12:17 +0200

    Functions Intermediate2 assignment

--- python_stack/python/fundamentals/Functions _Intermediate_II/Functions _Intermediate_II.py
+++ python_stack/python/fundamentals/Functions _Intermediate_II/Functions _Intermediate_II.py
@@ -10,3 +10,49 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
+x[1][0]=15
+print(x)
+
+students[0]['last_name']='bryant'
+print(students)
+
+sports_directory['soccer'][0]='Andress'
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+#2. iterate through ------------------------------------------
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(my_list):
+    for i in range(len(my_list)):
+        for y in my_list [i]:
+            print (y,'-', my_list[i][y])
+
+iterateDictionary(students)
+
+#3. Get Values------------------------------------------
+def iterateDictionary2(key_name, my_list):
+        for i in range(len(my_list)):
+            print(my_list[i][key_name])
+iterateDictionary2('first_name', students)
+
+#4. Iterate Through a Dictionary with List Values-------
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for x in some_dict:
+        print(len(some_dict[x]), x)
+        for value in some_dict[x]:
+            print(value)
+printInfo(dojo)
+
