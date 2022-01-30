a56f940502a544fba3d5283b2b15479e7f116aca
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-01-30 22:50:42 +0200

    renaming files

---
+++ python_stack/python/fundamentals/Functions _Intermediate_II/Functions _Intermediate_II.py
@@ -0,0 +1,12 @@
+#1. update-----------------------------------------
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
