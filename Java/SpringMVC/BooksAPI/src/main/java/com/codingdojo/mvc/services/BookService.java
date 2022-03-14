6c1c3e7ef528dda8f130f26414a052e1da896bd7
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-03-15 00:17:36 +0200

    Book API Assignment

---
+++ Java/SpringMVC/BooksAPI/src/main/java/com/codingdojo/mvc/services/BookService.java
@@ -0,0 +1,48 @@
+package com.codingdojo.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+ // adding the book repository as a dependency
+	private final BookRepository bookRepository;
+ 
+ public BookService(BookRepository bookRepository) {
+     this.bookRepository = bookRepository;
+ }
+ // returns all the books
+ public List<Book> allBooks() {
+     return bookRepository.findAll();
+ }
+ // creates a book
+ public Book createBook(Book b) {
+     return bookRepository.save(b);
+ }
+ // retrieves a book
+ public Book findBook(Long id) {
+     Optional<Book> optionalBook = bookRepository.findById(id);
+     if(optionalBook.isPresent()) {
+         return optionalBook.get();
+     } else {
+         return null;
+     }
+ }
+public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+	Book currentBook = findBook(id);
+	currentBook.setTitle(title);
+	currentBook.setDescription(desc);
+	currentBook.setLanguage(lang);
+	currentBook.setNumberOfPages(numOfPages);
+	return bookRepository.save(currentBook);
+}
+public void deleteBook(Long id) {
+	bookRepository.deleteById(id);
+	
+}
+}
+
