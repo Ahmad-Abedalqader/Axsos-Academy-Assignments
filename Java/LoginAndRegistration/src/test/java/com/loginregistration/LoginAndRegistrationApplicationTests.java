58fc52f65a70be5eee50fffd027c39e45b66cef4
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-03-21 16:32:20 +0200

    loin and registration assignment

---
+++ Java/LoginAndRegistration/src/test/java/com/loginregistration/LoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
